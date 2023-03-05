a7999749332396ce4bb7685c17791681a451358a
CQBerry <cqbery@gmail.com>
2023-03-05 15:11:04 +0800

    fix: cannot build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://front.uninetwork.top/",
   integrations: [
     tailwind(),
     image({
